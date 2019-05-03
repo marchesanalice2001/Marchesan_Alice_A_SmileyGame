b142fc6f743305e18a16aa08379626bf5e2ef3c0
alice <alicemarchesan2001@gmail.com>
2019-05-03 19:20:42 +0200

    3-05-2019 continuazione programma
    inserimento dei caratteri speciali e ricontrollo dei metodi... disfunzionamento nel controllo e nella parte del giocatore

--- Smiley_Game/Smiley_Game.ino
+++ Smiley_Game/Smiley_Game.ino
@@ -1,182 +1,189 @@
-int vai = 0;// mi dice quando ho premuto qualcosa  // vedo se si puo togliere
-int vite = 3;// numero di vite che ho a disposizione
-int tempo = 0; // tempo da ritornare alla fine
-int posizione = 0;
-int punteggio = 0; // punteggio fatto dal giocatore
-int controllo = 0;
-#define B1 13
-#define B2 10
-#define B3 8
+#define B1 7
+#define B2 8
+#define B3 13
 #include <LiquidCrystal.h>
 #include <avr/io.h>
 #include <avr/wdt.h>
 #define Reset_AVR()   wdt_enable(WDTO_30MS); //reset
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+int vai = 0;// mi dice quando ho premuto qualcosa
+int vite = 3;// numero di vite che ho a disposizione
+float tempo = 0; // tempo da ritornare alla fine
+int posizione = 0;
+int punteggio = 0; // punteggio fatto dal giocatore
+int controllo = 0;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+byte Smile[8] = {B00000,B01010,B01010,B00000,B10001,B11011,B01110,B00000};
+byte Cuore[8] = {B00000,B01010,B11111,B11111,B11111,B01110,B00100,B00000};
+byte Cattivo[8] = {B00000,B01010,B01010,B00000,B01110,B11011,B10001,B00000};
 
 void setup() {
 Serial.begin(9600);
 randomSeed(millis());
-  pinMode(B1, INPUT);
-  pinMode(B2, INPUT);
-  pinMode(B3, INPUT);
+  pinMode(B1, INPUT_PULLUP);
+  pinMode(B2, INPUT_PULLUP);
+  pinMode(B3, INPUT_PULLUP);
   pinMode(A0, OUTPUT);
-lcd.clear();
-lcd.begin(16, 2);
-
+  lcd.clear();
+  lcd.createChar(0, Smile);
+  lcd.createChar(1, Cuore);
+  lcd.createChar(2, Cattivo);
+  lcd.begin(16, 2);
 }
+
 void play()//metodo iniziale che fa partire il gioco appena premo un bottone 
-{
-  scrivi("   SMILEY GAME    ","PREMI PER GIOCARE");
-  while(digitalRead (B1) == LOW ||digitalRead (B2) == LOW || digitalRead (B3) == LOW)
+{ 
+  scrivi2("  SMILEY GAME    "," PREMI E GIOCA ");
+  while(vai == 0)
   {
-    vai = 1;
-    tempo = millis;
+    if(digitalRead (B1) == LOW || digitalRead (B2) == LOW || digitalRead (B3) == LOW)
+    {
+       vai = 1;
+       tempo = millis();
+    }
   }
 }
-void ritorno()//metodo finale che mi restituisce il tempo in cui ho giocato e il punteggio
-{
-  scrivi("GAME OVER","SCORE "+ String(punteggio) + "   TIME "+String(tempo/1000));
-  delay(4000);
-  Reset_AVR(); 
-}
-void scrivi ( String n, String m) // metodo che scrive sul'lcd
+void scrivi2 ( String n, String m) // metodo che scrive sul'lcd
 {
       lcd.begin(16, 2);
       lcd.print(n);
       lcd.setCursor(0, 1);
       lcd.print(m);
 }
-void cattivo()
+void ritorno()//metodo finale che mi restituisce il tempo in cui ho giocato e il punteggio
 {
-  int conto = millis;
-  int controllo = 0;
-  controlla(mostraCattivo());
-  bravoocattivo(controllo);
+  scrivi2("      GAME","      OVER");delay(2000);
+  scrivi2("SCORE "+ String(punteggio), " TIME "+String(tempo/1000));
+  delay(4000);
+  Reset_AVR(); 
 }
-void buono()
+void scrivi ( String n, int v, int p,int b) // metodo che scrive sul'lcd
 {
-  int conto = millis;
-  controlla(mostraBuono());
-  bravoocattivo(controllo);
+      lcd.begin(16, 2);
+      lcd.print(n);
+      mettivite(v);
+      lcd.setCursor(0, 1);
+      if(b==0)
+      {
+        mettifaccine(p,0);
+      }
+      if(b==1)
+      {
+        mettifaccine(p,2);
+      }
 }
-int mostraCattivo()
+void mettifaccine(int p, int n)
 {
-    int o = (analogRead(A1) %3);
-    if(o == 0 )
-    {
-      scrivi("SCORE:"+String(punteggio)+"        "+inseriscicuori(vite),"");
-    }
-    if(o == 1)
-    {
-      scrivi("SCORE:"+String(punteggio)+"        "+inseriscicuori(vite),"");
-    }
-    if(o == 2)
-    {
-      scrivi("SCORE:"+String(punteggio)+"        "+inseriscicuori(vite),"");
-    }
-    return o;
+  if(p==0)
+      { 
+        lcd.setCursor(3,1);
+        lcd.write(byte(n));
+      }
+      if(p==1)
+      {
+        lcd.setCursor(7,1);
+        lcd.write(byte(n)); 
+      }
+      if(p==2)
+      {
+        lcd.setCursor(11,1);
+        lcd.write(byte(n));
+      }
 }
-int mostraBuono()
+void mettivite(int v)
 {
-    int o = (analogRead(A1) %3);
-    if(o == 0 )
-    {
-      scrivi("SCORE:"+String(punteggio)+"        "+inseriscicuori(vite),"");
-    }
-    if(o == 1)
-    {
-      scrivi("SCORE:"+String(punteggio)+"        "+inseriscicuori(vite),"");
-    }
-    if(o == 2)
-    {
-      scrivi("SCORE:"+String(punteggio)+"        "+inseriscicuori(vite),"");
-    }
-    return o;
+   for(int i= 0;i<v;i++)
+      {
+        lcd.write(byte(1));
+      }
 }
 
+void cattivo()
+{ int o = (analogRead(A1) %3);
+  scrivi("SCORE:"+String(punteggio)+"   ",vite,o,0); 
+  int controllo = 0;
+  controlla(o);
+  bravoocattivo(0);
+}
+void buono()
+{ int o = (analogRead(A1) %3);
+  scrivi("SCORE:"+String(punteggio)+"   ",vite,o,1);
+  controlla(o);
+  bravoocattivo(1);
+}
 void controlla(int posto)
 {
-  int secondi = millis;
-  while((millis - secondi)<1000 && (digitalRead (B1) == HIGH) ||(digitalRead (B2) == HIGH) || (digitalRead (B3) == HIGH))
+  int secondi = millis();
+  while((millis() - secondi)<=4000 || (digitalRead (B1) == LOW) ||(digitalRead (B2) == LOW) || (digitalRead (B3) == LOW))
   {
-    if(digitalRead (B1) == HIGH)
+    if(digitalRead (B1) == LOW)
     {
       posizione = 0;
     }
-    if(digitalRead (B2) == HIGH)
+    if(digitalRead (B2) == LOW)
     {
       posizione = 1;
     }
-    if(digitalRead (B3) == HIGH)
+    if(digitalRead (B3) == LOW)
     {
       posizione = 2;
     }
+  }
     if(posto == posizione)
     {
       controllo = 1;
     }
-    else{
+    else
+    {
       controllo = 0;
     }
-  }
 }
-
-String inseriscicuori(int v)
+void bravoocattivo(int n)
 {
-  String s = "";
-  while(v >= 0)
+  bool finito = false;
+  while(!finito)
   {
-    s = s +"❤";
+  if(controllo == 0 && n ==1 || controllo == 1 && n == 0)
+  {
+    tolgo(); finito = true;
   }
-  return s;
-}
-
-void bravoocattivo(int h)
-{
-  if(h ==0)
+  if(controllo == 1 && n == 1 || controllo == 0 && n == 0)
   {
-    tolgo();
-  }
-  if(h ==1)
-  {
-    aggiungo();
+    aggiungo(); finito = true;
   }
 }
-
+controllo = 0;posizione = 0;
+}
 void tolgo()
 {
-  vite = vite - 1;
-  if(punteggio > 0)
+  vite = vite--;
+  if(punteggio >10)
   {
     punteggio = punteggio-10;
   }
 }
 void aggiungo()
 {
-  if(vite<3)
+  if(vite < 3)
   {
-    vite = vite+1;
+    vite = vite++;
   }
   punteggio = punteggio +10;
 }
-
-
 void loop() 
 {
+  digitalWrite(A0, HIGH);
   play();
-  while(vai == 1 && vite > 0)
+  while(vite > 0)
   {
     int r = (analogRead(A1) %2);
     if(r == 1)
     {
-      cattivo();
+      cattivo();delay(1000);
     }
-    if(r == 0)
+    else
     {
-      buono();
+      buono();delay(1000);
     }
   }
   ritorno();
 }
-
-
