a23c79759f56e4f026785567ef0bbdd13542e808
alice <alicemarchesan2001@gmail.com>
2019-05-10 09:19:16 +0200

    dal 7-05-2019 al 9-05-2019
    completamento programma, unica cosa e che non fa visualizzare bene le faccine i cuori si

--- Smiley_Game/Smiley_Game.ino
+++ Smiley_Game/Smiley_Game.ino
@@ -8,15 +8,15 @@
 int vai = 0;// mi dice quando ho premuto qualcosa
 int vite = 3;// numero di vite che ho a disposizione
 float tempo = 0; // tempo da ritornare alla fine
-int posizione = 0;
+int posizione = 3;
 int punteggio = 0; // punteggio fatto dal giocatore
 int controllo = 0;
-int timer = 3000;
+int timer = 4000;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 byte Smile[8] = {B00000,B01010,B01010,B00000,B10001,B11011,B01110,B00000};
 byte Cuore[8] = {B00000,B01010,B11111,B11111,B11111,B01110,B00100,B00000};
 byte Cattivo[8] = {B00000,B01010,B01010,B00000,B01110,B11011,B10001,B00000};
-
+byte CuoreRotto[8] = { B00000,B01010,B10101, B10001,B10001,B01010,B00100,B00000 };
 void setup() {
 Serial.begin(9600);
 randomSeed(millis());
@@ -25,180 +25,127 @@ randomSeed(millis());
   pinMode(B3, INPUT_PULLUP);
   pinMode(A0, OUTPUT);
   lcd.clear();
-  lcd.createChar(0, Smile);
-  lcd.createChar(1, Cuore);
+  lcd.createChar(1, Smile);
+  lcd.createChar(0, Cuore);
   lcd.createChar(2, Cattivo);
+  lcd.createChar(3, CuoreRotto);
   lcd.begin(16, 2);
 }
-
 void play()//metodo iniziale che fa partire il gioco appena premo un bottone 
-{ 
-  scrivi2("  SMILEY GAME    "," PREMI E GIOCA ");
+{ scrivi2("  SMILEY GAME    "," PREMI E GIOCA ");
   while(vai == 0)
-  {
-    if(digitalRead (B1) == LOW || digitalRead (B2) == LOW || digitalRead (B3) == LOW)
-    {
-       vai = 1;
+  { if(digitalRead (B1) == LOW || digitalRead (B2) == LOW || digitalRead (B3) == LOW)
+    {   vai = 1;
        tempo = millis();
     }
   }
 }
 void scrivi2 ( String n, String m) // metodo che scrive sul'lcd
-{
-      lcd.begin(16, 2);
+{     lcd.begin(16, 2);
       lcd.print(n);
       lcd.setCursor(0, 1);
       lcd.print(m);
 }
 void ritorno()//metodo finale che mi restituisce il tempo in cui ho giocato e il punteggio
-{ 
-  scrivi2("      GAME","      OVER");delay(3000);
-  scrivi2("SCORE "+ String(punteggio), " TIME "+String((millis()-tempo)/1000)+"sec");
+{ scrivi2("      GAME","      OVER");delay(3000);
+  scrivi2(" SCORE "+ String(punteggio), " TIME "+String((millis()-tempo)/1000)+"sec");
   delay(4000);
   Reset_AVR(); 
 }
 void scrivi ( String n, int v, int p,int b) // metodo che scrive sul'lcd
-{
-      lcd.begin(16, 2);
+{     lcd.begin(16, 2);
       lcd.print(n);
       mettivite(v);
       if(b==0)
-      {
-        mettifaccine(p,0);
-      }
+      { mettifaccine(p,1);  }
       if(b==1)
-      {
-        mettifaccine(p,2);
-      }
+      { mettifaccine(p,2);  }
 }
 void mettifaccine(int p, int n)
-{
-  if(p==0)
-      { 
-        lcd.setCursor(3,1);
+{     if(p==0)
+      { lcd.setCursor(3,1);
         lcd.write(byte(n));
       }
       if(p==1)
-      {
-        lcd.setCursor(7,1);
+      { lcd.setCursor(7,1);
         lcd.write(byte(n)); 
       }
       if(p==2)
-      {
-        lcd.setCursor(11,1);
+      { lcd.setCursor(11,1);
         lcd.write(byte(n));
       }
 }
 void mettivite(int vita)
-{
-   for(int i= 0;i<vita;i++)
-      {
-        lcd.write(byte(1));
-      }
+{     for(int i= 0;i<vita;i++)
+      { lcd.write(byte(0));}
+      for(int i= 0;i<(3-vita);i++)
+      { lcd.write(byte(3));}
 }
-
 void cattivo()
 { int o = (analogRead(A1) %3);
-  scrivi("SCORE:"+String(punteggio)+"    ",vite,o,1); 
-  controlla(o);
+  scrivi("SCORE:"+String(punteggio)+"   ",vite,o,1); 
+  controlla(o, 0);
   bravoocattivo(0);
 }
 void buono()
 { int o = (analogRead(A1) %3);
-  scrivi("SCORE:"+String(punteggio)+"    ",vite,o,0);
-  controlla(o);
+  scrivi("SCORE:"+String(punteggio)+"   ",vite,o,0);
+  controlla(o, 1);
   bravoocattivo(1);
 }
-void controlla(int posto)
-{
-  int secondi = millis();
-  while((millis() - secondi)<=timer || (digitalRead (B1) == LOW) ||(digitalRead (B2) == LOW) || (digitalRead (B3) == LOW))
-  {
+void controlla(int posto, int b)
+{ bool finito = false; float sec = millis();
+  while(!finito)
+  {  if((millis() - sec) >= timer)
+    { posizione = 3; finito = true; contatimer(); }
     if(digitalRead (B1) == LOW)
-    {
-      posizione = 0;
-    }
+    { posizione = 0; finito = true; contatimer(); }
     if(digitalRead (B2) == LOW)
-    {
-      posizione = 1;
-    }
+    { posizione = 1; finito = true; contatimer(); }
     if(digitalRead (B3) == LOW)
-    {
-      posizione = 2;
-    }
-  }
-    uguale(posto);
-    contatimer();
+    { posizione = 2; finito = true; contatimer(); }
+  }   
+    scrivi2("SCORE:"+String(punteggio),"    "); delay(1000);
+    uguale(posto, b);
 }
-void uguale(int pos)
-{
-    if(pos == posizione)
-    {
-      controllo = 1;
-    }
-    else
-    {
-      controllo = 0;
-    }
+void uguale(int pos, int boc)
+{   if((boc == 0 && posizione != 3 )|| pos == posizione)
+    { controllo = 1;  }
+    if((boc == 0 && posizione == 3) || (pos != posizione && boc == 1))
+    { controllo = 0;  }
 }
 void contatimer()
-{
-  if(timer > 400)
-  {
-    timer = timer -100;
-  }
+{ if(timer <= 1000)
+  { timer = 4000; }
   else
-  {
-    timer = 3000;
-  }
+  { timer = timer - 200; }
 }
 void bravoocattivo(int n)
-{
-  bool finito = false;
-  while(!finito)
-  {
-  if(controllo == 0 && n ==1 || controllo == 1 && n == 0)
-  {
-    tolgo(); finito = true;
-  }
+{ if(controllo == 0 && n ==1 || controllo == 1 && n == 0)
+  { tolgo();  }
   if(controllo == 1 && n == 1 || controllo == 0 && n == 0)
-  {
-    aggiungo(); finito = true;
-  }
-}
-controllo = 0;posizione = 0;
+  { aggiungo(); }
+controllo = 0; posizione = 3;
 }
 void tolgo()
-{
-  vite = vite-1;
-  if(punteggio >0)
-  {
-    punteggio = punteggio-10;
-  }
+{ vite = vite-1;
+  if(punteggio >= 10)
+  { punteggio = punteggio-10; }
 }
 void aggiungo()
-{
-  if(vite < 3)
-  {
-    vite = vite+1;
-  }
+{ if(vite < 3)
+  { vite = vite+1; }
   punteggio = punteggio +10;
 }
 void loop() 
-{
-  digitalWrite(A0, HIGH);
-  play();
+{ digitalWrite(A0, HIGH);
+  play();delay(1000);
   while(vite > 0)
-  { delay(1000);
-    int r = (analogRead(A1) %2);
+  { int r = (analogRead(A1) %2);
     if(r == 1)
-    {
-      cattivo();delay(1000);
-    }
+    { cattivo();}
     else
-    {
-      buono();delay(1000);
-    }
+    { buono();}
   }
   ritorno();
 }
